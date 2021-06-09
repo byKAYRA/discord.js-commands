024e9272c721ecc864f56d8be2f1c1be0816c2ee
Kayra Onur <77785551+ByKayra@users.noreply.github.com>
2021-06-09 11:09:18 +0300

    Database'ye kaydettiğiniz verileri sıralar

--- commands/db-sıralama.js
+++ commands/db-sıralama.js
@@ -4,13 +4,13 @@ const db = require("quick.db");
 
 module.exports.run = async (bot, message, params) => {
 
-  let money = db.startsWith(`bakiyecdare-`, { sort: '.data'})
+  let money = db.startsWith(`bakiyecdare-`, { sort: '.data'}) //kaydetme şeklini yazın Örnek: bakiyecdare-${message.author.id} yazdıysanız bakiyecdare- yazmalısını<
   let content = "";
 
   for (let i = 0; i < 10; i++) {
     let user = bot.users.cache.get(money[i].ID.split('_')[2])
 
-    content += `**${i+1})** <@!${money[i].ID.split('-')[1]}> | **${money[i].data}**TL\n`
+    content += `**${i+1})** <@!${money[i].ID.split('-')[1]}> | **${money[i].data}**TL\n` //<@!${money[i].ID.split('-')[1]}> kısmındaki split('-') i database adı ile message.author.id arasındaki tanım yapmalısınız Örn: bakiyecdare-${message.author.id} yazdıysam - yazmalıyım 
   }
 
   const embed = new Discord.MessageEmbed()
