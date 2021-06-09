a44f3fe3355e0d86f1cf60588ef3ad99fa3efbd0
Kayra Onur <77785551+ByKayra@users.noreply.github.com>
2021-06-09 11:11:19 +0300

    Database'ye kaydettiğiniz verileri sıralar

--- commands/db-sıralama.js
+++ commands/db-sıralama.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, params) => {
   let money = db.startsWith(`bakiyecdare-`, { sort: '.data'}) //kaydetme şeklini yazın Örnek: bakiyecdare-${message.author.id} yazdıysanız bakiyecdare- yazmalısını<
   let content = "";
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 10; i++) { //bu kısımdaki 10 mesajda kaç kişiyi göstereceğini belirler
     let user = bot.users.cache.get(money[i].ID.split('_')[2])
 
     content += `**${i+1})** <@!${money[i].ID.split('-')[1]}> | **${money[i].data}**TL\n` //<@!${money[i].ID.split('-')[1]}> kısmındaki split('-') i database adı ile message.author.id arasındaki tanım yapmalısınız Örn: bakiyecdare-${message.author.id} yazdıysam - yazmalıyım 
